74f2638ebd6cfd69c5a403fd4995cdd540e56d69
Jappowski <58007440+Jappowski@users.noreply.github.com>
2020-01-19 20:48:54 +0100

    Adding drow system

--- TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/MainWindow.xaml.cs
@@ -29,8 +29,8 @@ namespace WpfApp3
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Button button = (Button)sender;
-            if(turn)
-            { 
+            if (turn)
+            {
                 button.Content = "X";
                 button.IsEnabled = false;
             }
@@ -42,51 +42,55 @@ namespace WpfApp3
             turn = !turn;
             count++;
             isWin();
-        }
-
-        private void isWin()
-        {
-            bool isWiner = false;
-            //row checking
-            if ((!A1.IsEnabled) &&A1.Content.Equals(A2.Content) && A2.Content.Equals(A3.Content) && A3.Content.Equals(A4.Content) && A3.Content.Equals(A4.Content) && A4.Content.Equals(A5.Content))
-                isWiner = true;
-            if ((!B1.IsEnabled) && B1.Content.Equals(B2.Content) && B2.Content.Equals(B3.Content) && B3.Content.Equals(B4.Content) && B3.Content.Equals(B4.Content) && B4.Content.Equals(B5.Content))
-                isWiner = true;
-            if ((!C1.IsEnabled) && C1.Content.Equals(C2.Content) && C2.Content.Equals(C3.Content) && C3.Content.Equals(C4.Content) && C3.Content.Equals(C4.Content) && C4.Content.Equals(C5.Content))
-                isWiner = true;
-            if ((!D1.IsEnabled) && D1.Content.Equals(D2.Content) && D2.Content.Equals(D3.Content) && D3.Content.Equals(D4.Content) && D3.Content.Equals(D4.Content) && D4.Content.Equals(D5.Content))
-                isWiner = true;
-            if ((!E1.IsEnabled) && E1.Content.Equals(E2.Content) && E2.Content.Equals(E3.Content) && E3.Content.Equals(E4.Content) && E3.Content.Equals(E4.Content) && E4.Content.Equals(E5.Content))
-                isWiner = true;
-            //Column checking
-            if ((!A1.IsEnabled) && A1.Content.Equals(B1.Content) && B1.Content.Equals(C1.Content) && C1.Content.Equals(D1.Content) && D1.Content.Equals(E1.Content))
-                isWiner = true;
-            if ((!A2.IsEnabled) && A2.Content.Equals(B2.Content) && B2.Content.Equals(C2.Content) && C2.Content.Equals(D2.Content) && D2.Content.Equals(E2.Content))
-                isWiner = true;
-            if ((!A3.IsEnabled) && A3.Content.Equals(B3.Content) && B3.Content.Equals(C3.Content) && C3.Content.Equals(D3.Content) && D3.Content.Equals(E3.Content))
-                isWiner = true;
-            if ((!A4.IsEnabled) && A4.Content.Equals(B4.Content) && B4.Content.Equals(C4.Content) && C4.Content.Equals(D4.Content) && D4.Content.Equals(E4.Content))
-                isWiner = true;
-            if ((!A5.IsEnabled) && A5.Content.Equals(B5.Content) && B5.Content.Equals(C5.Content) && C5.Content.Equals(D5.Content) && D5.Content.Equals(E5.Content))
-                isWiner = true;
-            //Cross checking
-            if ((!A1.IsEnabled) && A1.Content.Equals(B2.Content) && B2.Content.Equals(C3.Content) && C3.Content.Equals(D4.Content) && D4.Content.Equals(E5.Content))
-                isWiner = true;
-            if ((!E1.IsEnabled) && E1.Content.Equals(D2.Content) && D2.Content.Equals(C3.Content) && C3.Content.Equals(B4.Content) && B4.Content.Equals(A5.Content))
-                isWiner = true;
 
 
-
-
-            if (isWiner)
+            void isWin()
             {
-                string winner = "";
-                if (turn)
-                    winner = "O";
-                else
-                    winner = "X";
+                bool isWiner = false;
+                //row checking
+                if ((!A1.IsEnabled) && A1.Content.Equals(A2.Content) && A2.Content.Equals(A3.Content) && A3.Content.Equals(A4.Content) && A3.Content.Equals(A4.Content) && A4.Content.Equals(A5.Content))
+                    isWiner = true;
+                if ((!B1.IsEnabled) && B1.Content.Equals(B2.Content) && B2.Content.Equals(B3.Content) && B3.Content.Equals(B4.Content) && B3.Content.Equals(B4.Content) && B4.Content.Equals(B5.Content))
+                    isWiner = true;
+                if ((!C1.IsEnabled) && C1.Content.Equals(C2.Content) && C2.Content.Equals(C3.Content) && C3.Content.Equals(C4.Content) && C3.Content.Equals(C4.Content) && C4.Content.Equals(C5.Content))
+                    isWiner = true;
+                if ((!D1.IsEnabled) && D1.Content.Equals(D2.Content) && D2.Content.Equals(D3.Content) && D3.Content.Equals(D4.Content) && D3.Content.Equals(D4.Content) && D4.Content.Equals(D5.Content))
+                    isWiner = true;
+                if ((!E1.IsEnabled) && E1.Content.Equals(E2.Content) && E2.Content.Equals(E3.Content) && E3.Content.Equals(E4.Content) && E3.Content.Equals(E4.Content) && E4.Content.Equals(E5.Content))
+                    isWiner = true;
+                //Column checking
+                if ((!A1.IsEnabled) && A1.Content.Equals(B1.Content) && B1.Content.Equals(C1.Content) && C1.Content.Equals(D1.Content) && D1.Content.Equals(E1.Content))
+                    isWiner = true;
+                if ((!A2.IsEnabled) && A2.Content.Equals(B2.Content) && B2.Content.Equals(C2.Content) && C2.Content.Equals(D2.Content) && D2.Content.Equals(E2.Content))
+                    isWiner = true;
+                if ((!A3.IsEnabled) && A3.Content.Equals(B3.Content) && B3.Content.Equals(C3.Content) && C3.Content.Equals(D3.Content) && D3.Content.Equals(E3.Content))
+                    isWiner = true;
+                if ((!A4.IsEnabled) && A4.Content.Equals(B4.Content) && B4.Content.Equals(C4.Content) && C4.Content.Equals(D4.Content) && D4.Content.Equals(E4.Content))
+                    isWiner = true;
+                if ((!A5.IsEnabled) && A5.Content.Equals(B5.Content) && B5.Content.Equals(C5.Content) && C5.Content.Equals(D5.Content) && D5.Content.Equals(E5.Content))
+                    isWiner = true;
+                //Cross checking
+                if ((!A1.IsEnabled) && A1.Content.Equals(B2.Content) && B2.Content.Equals(C3.Content) && C3.Content.Equals(D4.Content) && D4.Content.Equals(E5.Content))
+                    isWiner = true;
+                if ((!E1.IsEnabled) && E1.Content.Equals(D2.Content) && D2.Content.Equals(C3.Content) && C3.Content.Equals(B4.Content) && B4.Content.Equals(A5.Content))
+                    isWiner = true;
 
-                MessageBox.Show(winner + " win a game!");
+
+
+
+                if (isWiner)
+                {
+                    string winner = "";
+                    if (turn)
+                        winner = "O";
+                    else
+                        winner = "X";
+
+                    MessageBox.Show(winner + " win a game!");
+                }
+                else if (!isWiner && count == 25)
+                    MessageBox.Show("DRAW!");
+                    
             }
         }
     }
