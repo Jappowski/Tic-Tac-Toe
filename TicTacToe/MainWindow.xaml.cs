5bcf1bbb3e69f97e14d91eed70321b7bfe65e65a
Pastooh22 <48555920+Pastooh22@users.noreply.github.com>
2020-01-20 13:36:46 +0100

    Blocking system

--- TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/MainWindow.xaml.cs
@@ -43,7 +43,6 @@ namespace WpfApp3
             count++;
             isWin();
 
-
             void isWin()
             {
                 bool isWiner = false;
@@ -75,9 +74,6 @@ namespace WpfApp3
                 if ((!E1.IsEnabled) && E1.Content.Equals(D2.Content) && D2.Content.Equals(C3.Content) && C3.Content.Equals(B4.Content) && B4.Content.Equals(A5.Content))
                     isWiner = true;
 
-
-
-
                 if (isWiner)
                 {
                     string winner = "";
@@ -87,11 +83,44 @@ namespace WpfApp3
                         winner = "X";
 
                     MessageBox.Show(winner + " has won the game!");
+                    buttonDisabled();
                 }
                 else if (!isWiner && count == 25)
+                {
                     MessageBox.Show("DRAW!");
-                    
+                    buttonDisabled();
+                }
             }
         }
+        void buttonDisabled()
+        {
+            A1.IsEnabled = false;
+            A2.IsEnabled = false;
+            A3.IsEnabled = false;
+            A4.IsEnabled = false;
+            A5.IsEnabled = false;
+            B1.IsEnabled = false;
+            B2.IsEnabled = false;
+            B3.IsEnabled = false;
+            B4.IsEnabled = false;
+            B5.IsEnabled = false;
+            C1.IsEnabled = false;
+            C2.IsEnabled = false;
+            C3.IsEnabled = false;
+            C4.IsEnabled = false;
+            C5.IsEnabled = false;
+            D1.IsEnabled = false;
+            D2.IsEnabled = false;
+            D3.IsEnabled = false;
+            D4.IsEnabled = false;
+            D5.IsEnabled = false;
+            E1.IsEnabled = false;
+            E2.IsEnabled = false;
+            E3.IsEnabled = false;
+            E4.IsEnabled = false;
+            E5.IsEnabled = false;
+
+        }
+
     }
 }
