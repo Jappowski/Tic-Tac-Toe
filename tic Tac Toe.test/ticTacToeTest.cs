94793567ef7977113d93640b87bf03e902383296
Pastooh22 <48555920+Pastooh22@users.noreply.github.com>
2020-01-22 23:45:05 +0100

    Unit Tests

--- tic Tac Toe.test/ticTacToeTest.cs
+++ tic Tac Toe.test/ticTacToeTest.cs
@@ -1,13 +1,26 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using WpfApp3;
 
 namespace tic_Tac_Toe.test
 {
-    public class ticTacToeTests
+    [TestClass]
+    public class UnitTest1
     {
-        void execute()
+        MainWindow mainwindow = new MainWindow();
+        [TestMethod]
+        public void TestMethod1()
         {
-
+            Assert.AreEqual(true, MainWindow.turn, "ruch X");
+            Assert.AreEqual(false, MainWindow.turn, "ruch O");
         }
-        
+    
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(false, MainWindow.grid1, "przyciski sa wylaczone");
+        }
     }
 }
